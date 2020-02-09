872ebf4d6410ce9e94e91afd334ec77b9e65df26
Andrii Leonov <andriileonov74@gmail.com>
2020-02-09 16:46:35 +0200

    Создан тест для нового контакта

---
+++ contact.py
@@ -0,0 +1,9 @@
+
+
+class Contact:
+
+    def __init__(self, first_name, last_name, address, email):
+        self.firstname = first_name
+        self.lastname = last_name
+        self.address = address
+        self.email = email
