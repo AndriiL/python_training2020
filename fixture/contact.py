271e529d80032592f2d098580b1e72e33706da6e
Andrii Leonov <andriileonov74@gmail.com>
2020-02-16 15:16:07 +0200

    Модификация группы и контакта 3_1

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,33 +7,52 @@ class ContactHelper:
 
     def create(self, contact):
         wd = self.app.wd
+        self.app.open_home_page()
         # init contact creation
         wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
+        self.fill_contact_form(contact)
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_to_home_page()
+        self.app.open_home_page()
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("address", contact.address)
+        self.change_field_value("email", contact.email)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_contact(self):
         wd = self.app.wd
         self.app.open_home_page()
-        # select first user
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         # submit delete
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+        self.app.open_home_page()
+
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_contact(self, new_contact_data):
+        wd = self.app.wd
+        self.app.open_home_page()
+        self.select_first_contact()
+        # open modification form
+        wd.find_element_by_name("selected[]").click()
+        # fill contact form
+        wd.find_element_by_xpath("//img[@title='Edit']").click()
+        self.fill_contact_form(new_contact_data)
+        # submit modification
+        wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
         self.app.open_home_page()
 
     def return_to_home_page(self):
--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@
 
 class Contact:
 
-    def __init__(self, first_name, last_name, address, email):
+    def __init__(self, first_name=None, last_name=None, address=None, email=None):
         self.firstname = first_name
         self.lastname = last_name
         self.address = address
