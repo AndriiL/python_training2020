d6c21a452799e277fa3cf3e4ae16ffecff5900ec
Andrii Leonov <andriileonov74@gmail.com>
2020-02-20 13:14:29 +0200

    Создание интелектуальной фикстуры 3_2

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,13 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(first_name="first_name", last_name="last_name", address="address", email="email"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(first_name="", last_name="", address="", email=""))
-    app.session.logout()
 
