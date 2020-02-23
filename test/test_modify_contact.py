703e310a27d4117e7d581820650f43e4b2d74419
Andrii Leonov <andriileonov74@gmail.com>
2020-02-23 13:13:10 +0200

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий 3_4

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_contact_first_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test_name"))
     app.contact.modify_first_contact(Contact(first_name="New first name"))
 
 
 def test_modify_contact_last_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test_name"))
     app.contact.modify_first_contact(Contact(last_name="New last name"))
