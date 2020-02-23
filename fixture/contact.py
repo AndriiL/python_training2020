fabf9e0329100b5e2a5ffbb660e58eeb5dc449e0
Andrii Leonov <andriileonov74@gmail.com>
2020-02-23 16:10:00 +0200

    Повышаем "интеллектуальность" навигации: избавляемся от лишних переходов для контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -17,10 +17,10 @@ class ContactHelper:
 
     def open_user_creation_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not(wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def fill_contact_form(self, contact):
-        wd = self.app.wd
         self.change_field_value("firstname", contact.firstname)
         self.change_field_value("lastname", contact.lastname)
         self.change_field_value("address", contact.address)
@@ -53,11 +53,16 @@ class ContactHelper:
         # open modification form
         wd.find_element_by_name("selected[]").click()
         # fill contact form
-        wd.find_element_by_xpath("//img[@title='Edit']").click()
+        self.select_modify_user()
         self.fill_contact_form(new_contact_data)
         # submit modification
         wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
         self.app.open_home_page()
+
+    def select_modify_user(self):
+        wd = self.app.wd
+        if not(wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("update")) > 0):
+            wd.find_element_by_xpath("//img[@title='Edit']").click()
 
     def return_to_home_page(self):
         wd = self.app.wd
